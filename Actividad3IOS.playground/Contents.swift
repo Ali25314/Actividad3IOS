aa91c04c42672b00811f54ed5db68db6db6dab62
Ali25314 <78836439+Ali25314@users.noreply.github.com>
2021-02-17 14:36:05 -0600

    Add files via upload

---
+++ Actividad3IOS.playground/Contents.swift
@@ -0,0 +1,21 @@
+
+var numero = 3;
+let numero2:Int = 4
+
+var decimal = 9.5
+let decimal2:Float = 9.6
+
+var text = "Ali Abdala"
+let texto: String = "Actividad 3"
+
+var anios = (20)
+print ("Tengo \(anios) anios")
+
+
+
+//array
+var array = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//Dictionary
+var DiasSemana: Dictionary = [1:"Lunes", 2:"Martes", 3:"Miercoles", 4:"Jueves", 5:"Viernes", 6:"Sabado", 7:"Domingo"]
+print (DiasSemana)
